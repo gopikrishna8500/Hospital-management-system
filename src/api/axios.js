f54d5056c4efee09938f07a331c4997e086c91bf
gopikrishna8500 <gopikrishnadindu@gmail.com>
2026-07-24 15:55:40 +0530

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hospital-management-system-3-kceq.onrender.com/api",
+  baseURL: "https://hospital-management-system-3-ne6q.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
   return req;
 });
 
